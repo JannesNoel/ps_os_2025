30d787d56a2da4af36001133c3e0d9585d1bfb61
Jannes <j.schmott@gmail.com>
2025-03-12 08:46:52 +0100

    Completed first homework

--- exercise01/task1/template/game.c
+++ exercise01/task1/template/game.c
@@ -1,13 +1,27 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define ALIVE 0
+#define DEAD 1
+
+void runGame(bool** field1, bool** field2, int width, int height, int stepCounter, int steps);
+void saveCurrentFrame(bool** field, int width, int height, int step);
+int getSurroundingAlive(bool** field, int width, int height, int x, int y);
+
+bool allocatePlayingField(bool*** field, int width, int height);
+void initPlayingField(bool*** playingField, int width, int height, double density);
+void generatePBM(bool** playingField, int width, int height, int iteration);
+
+void freePlayingField(bool** playingField, int width);
 
 void printUsage(const char* programName) {
 	printf("usage: %s <width> <height> <density> <steps>\n", programName);
 }
 
 int main(int argc, char* argv[]) {
-	if(argc != 5) {
+	if (argc != 5) {
 		printUsage(argv[0]);
 		return EXIT_FAILURE;
 	}
@@ -26,7 +40,141 @@ int main(int argc, char* argv[]) {
 	// every time.
 	srand(time(NULL));
 
-	// TODO
+	bool** field1 = NULL;
+	bool** field2 = NULL;
+	bool alloc1Fail = allocatePlayingField(&field1, width, height);
+	bool alloc2Fail = allocatePlayingField(&field2, width, height);
+
+	if (alloc1Fail || alloc2Fail) {
+		freePlayingField(field1, width);
+		freePlayingField(field2, width);
+
+		exit(1);
+	}
+
+	initPlayingField(&field1, width, height, density);
+	saveCurrentFrame(field1, width, height, 0);
+
+	runGame(field1, field2, width, height, 1, steps);
+
+	freePlayingField(field1, width);
+	freePlayingField(field2, width);
 
 	return EXIT_SUCCESS;
+}
+
+void runGame(bool** field1, bool** field2, int width, int height, int stepCounter, int steps) {
+	int surroundingSum;
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			surroundingSum = getSurroundingAlive(field1, width, height, x, y);
+
+			bool isAlive = field1[x][y] == ALIVE;
+			if (isAlive && (surroundingSum < 2 || surroundingSum > 3)) {
+				field2[x][y] = DEAD;
+			} else if (!isAlive && surroundingSum == 3) {
+				field2[x][y] = ALIVE;
+			} else {
+				field2[x][y] = field1[x][y];
+			}
+		}
+	}
+
+	saveCurrentFrame(field2, width, height, stepCounter);
+	if (stepCounter < steps) runGame(field2, field1, width, height, ++stepCounter, steps);
+}
+
+int getSurroundingAlive(bool** field, int width, int height, int x, int y) {
+	int sum = 0;
+
+	int xLowerBound = x == 0 ? 0 : -1;
+	int xUpperBound = x == (width - 1) ? 0 : 1;
+
+	int yLowerBound = y == 0 ? 0 : -1;
+	int yUpperBound = y == (height - 1) ? 0 : 1;
+
+	for (int xDelta = xLowerBound; xDelta <= xUpperBound; xDelta++) {
+		for (int yDelta = yLowerBound; yDelta <= yUpperBound; yDelta++) {
+			if (xDelta == 0 && yDelta == 0) continue;
+			sum += field[x + xDelta][y + yDelta] == ALIVE ? 1 : 0;
+		}
+	}
+
+	return sum;
+}
+
+void saveCurrentFrame(bool** field, int width, int height, int step) {
+	char name[14];
+	snprintf(name, 14, "gol_%05d.pbm", step);
+
+	FILE* outFile = fopen(name, "w");
+	if (outFile == NULL) {
+		return;
+	}
+
+	fprintf(outFile, "P1\n");
+	fprintf(outFile, "%d %d\n", width, height);
+
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			fprintf(outFile, "%d", field[i][j]);
+		}
+		fprintf(outFile, "\n");
+	}
+
+	fclose(outFile);
+}
+
+bool allocatePlayingField(bool*** field, int width, int height) {
+	*field = malloc(width * sizeof(*field));
+	if (*field == NULL) {
+		fprintf(stderr, "Failed to allocate playing field");
+		return 1;
+	}
+
+	bool arrFailure = false;
+	for (int i = 0; i < width; i++) {
+		(*field)[i] = calloc(height, sizeof(**field));
+
+		if ((*field)[i] == NULL) {
+			arrFailure = true;
+			break;
+		}
+	}
+
+	if (arrFailure) {
+		fprintf(stderr, "Failed to allocate playing field");
+
+		for (int i = 0; i < width; i++) {
+			free((*field)[i]);
+		}
+
+		free(*field);
+		return 1;
+	}
+
+	return 0;
+}
+
+void initPlayingField(bool*** field1, int width, int height, double density) {
+	double randDoub;
+
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			randDoub = (double)rand() / RAND_MAX;
+			(*field1)[i][j] = randDoub < density ? ALIVE : DEAD;
+		}
+	}
+}
+
+void freePlayingField(bool** field, int width) {
+	if (field == NULL) {
+		return;
+	}
+
+	for (int i = 0; i < width; i++) {
+		free(field[i]);
+	}
+
+	free(field);
 }
