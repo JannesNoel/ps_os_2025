6efc22e175a22faadd3de15f54e479e7070d3044
Jannes <j.schmott@gmail.com>
2025-03-27 00:33:47 +0100

    Added comment to pipbuf line

--- exercise04/solved/task_3/server.c
+++ exercise04/solved/task_3/server.c
@@ -13,6 +13,7 @@
 #define FIFO_BASE_NAME "/tmp/fifo_csbb9456_"
 #define FIFO_BASE_NAME_LEN strlen(FIFO_BASE_NAME)
 
+// on the zid server, this isn't set in the limits.h header so this is a fallback which uses the save value as my mac (hope this works)
 #ifndef PIPE_BUF
     #define PIPE_BUF 512 
 #endif
