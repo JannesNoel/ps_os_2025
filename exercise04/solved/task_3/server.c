e64ad9dd7d806ff0965c62c09848e9dc09d175d2
Jannes <j.schmott@gmail.com>
2025-03-27 00:46:18 +0100

    incorporated common.h

--- exercise04/solved/task_3/server.c
+++ exercise04/solved/task_3/server.c
@@ -10,13 +10,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define FIFO_BASE_NAME "/tmp/fifo_csbb9456_"
-#define FIFO_BASE_NAME_LEN strlen(FIFO_BASE_NAME)
-
-// on the zid server, this isn't set in the limits.h header so this is a fallback which uses the save value as my mac (hope this works)
-#ifndef PIPE_BUF
-    #define PIPE_BUF 512 
-#endif
+#include "common.h"
 
 void makeFifoPaths(int argc, char* argv[], char** fifoPaths);
 void waitForConnection(char** fifoPaths, int* openFifoFds, int numClients);
